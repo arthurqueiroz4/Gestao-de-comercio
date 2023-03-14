aadbab81c7315c9bc4a9c40a29d928b3f0ee79e0
arthurqueiroz4 <arthurqueiroz@great.ufc.br>
2023-03-13 21:55:26 -0300

    atualizando main

--- src/main/java/br/com/projeto_poo/model/Login.java
+++ src/main/java/br/com/projeto_poo/model/Login.java
@@ -4,31 +4,36 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToOne;
 import javax.persistence.SequenceGenerator;
 
 @Entity
 @SequenceGenerator(allocationSize = 1,initialValue = 1,name = "seq_login",sequenceName = "seq_login") 
 public class Login {
-	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_login")
-	private Long idlogin;
+	private Long idLogin;
 	
 	private String usuario;
 	
 	private String senha;
+
+	private String cnpj;
 	
-	@OneToOne(mappedBy = "address")
-	private Estoque estoque;
-	
-	public Long getId() {
-		return idlogin;
+	public String getCnpj() {
+		return cnpj;
 	}
 
-	public void setId(Long id) {
-		this.idlogin = id;
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	public Long getIdLogin() {
+		return idLogin;
+	}
+
+	public void setIdLogin(Long idlogin) {
+		this.idLogin = idlogin;
 	}
 
 	public String getUsuario() {
