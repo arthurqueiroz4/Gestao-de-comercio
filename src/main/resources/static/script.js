bb90b9fc94a8c75e2392d5257d6355f917634818
Levy Stevam <stevamlevy@gmail.com>
2023-03-17 19:14:12 -0300

    add requisição

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,6 +1,14 @@
 
-
-pick = () =>{
-    user_email = document.getElementById("email")
-    console.log(user_email.value)
-}
+const formu = document.getElementById("form1");
+formu.addEventListener('submit', event =>{
+    event.preventDefault();
+    const formData = new FormData(formu);
+    const data = Object.fromEntries(formData);
+    fetch("validarlogin", {
+        method:"POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    }).then(resp => resp.json()).then(data => console.log(data))
+})
